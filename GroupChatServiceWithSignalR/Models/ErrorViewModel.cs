fba4b1ebe10f2883bd84e2f71cf03214751e49e8
sultanajannat <111264226+sultanajannat@users.noreply.github.com>
2023-09-29 07:30:30 +0600

    Add project files.

---
+++ GroupChatServiceWithSignalR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GroupChatServiceWithSignalR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
